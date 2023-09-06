c2fdc5c4322f76667fdf0fe78a00b432eb533aaa
Joe Howarth <josephehowarth@gmail.com>
2023-09-06 15:26:53 -0700

    scripts for devving

---
+++ maturin.sh
@@ -0,0 +1,17 @@
+#! /bin/zsh
+
+# Change to the simpy directory and activate its Python environment
+cd simpy || { echo "Failed to cd into simpy. Exiting."; exit 1; }
+
+# Get the path to the virtual environment
+venv_path=$(poetry env info --path) || { echo "Failed to get Poetry environment. Exiting."; exit 1; }
+
+# Print and activate the virtual environment
+echo "Virtual Environment Path: $venv_path"
+source "${venv_path}/bin/activate" || { echo "Failed to activate virtual environment. Exiting."; exit 1; }
+
+# Go back to the original directory
+cd .. || { echo "Failed to cd to parent directory. Exiting."; exit 1; }
+
+# Run cargo watch with maturin develop
+cargo watch -s "cd simpy; maturin develop" || { echo "Cargo watch failed. Exiting."; exit 1; }
